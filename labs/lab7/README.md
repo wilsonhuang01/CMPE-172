1b806a4f32551f029a533fdc107c687ddf7515ff
wilsonhuang01 <wilsonpwhuang@gmail.com>
2023-06-01 15:39:13 -0700

    init

---
+++ labs/lab7/README.md
@@ -0,0 +1,37 @@
+# CMPE 172 - Lab #7 Notes
+
+## Spring Payments
+
+![](images/01_sample1_desktop.png)
+![](images/02_sample1_browser.png)
+![](images/03_sample2_desktop.png)
+![](images/04_sample2_browser.png)
+![](images/05_sample3_desktop.png)
+![](images/06_sample3_browser.png)
+
+Sample Payment Screen outputs.
+
+## Discussion
+
+### Lombok
+
+Lombok annotations were used to generate methods
+like getters and setters in PaymentsCommand.java.
+
+### Thymeleaf
+
+Thymeleaf was used for the html view template. It
+includes directives such as href, object, field, and
+text.
+
+### Spring
+
+Some Spring features used in this lab include 
+Spring JPA and Spring Web.
+
+### Jackson
+
+Jackson is a JSON processor that can translates Java
+objects to JSON and JSON to Java objects. It was used
+in response files (e.g. AuthReponse and CaptureResponse)
+and Payload.java.
---
+++ labs/lab7/spring-cybersource/src/main/java/com/example/springcybersource/SpringCybersourceApplication.java
@@ -0,0 +1,103 @@
+package com.example.springcybersource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+
+@SpringBootApplication
+public class SpringCybersourceApplication implements CommandLineRunner {
+
+	private static boolean DEBUG = true ;
+
+    @Value("${cybersource.apihost}") private String apiHost ;
+    @Value("${cybersource.merchantkeyid}") private String merchantKeyId ;
+    @Value("${cybersource.merchantsecretkey}") private String merchantsecretKey ;
+    @Value("${cybersource.merchantid}") private String merchantId ;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCybersourceApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) {
+		
+        System.out.println("===== CYBERSOURCE PAYMENT TEST =====") ;
+
+        if (DEBUG) {
+         	System.out.println( apiHost ) ;
+        	System.out.println( merchantKeyId ) ;
+        	System.out.println( merchantsecretKey ) ;
+        	System.out.println( merchantId ) ;         	
+        }
+ 
+		CyberSourceAPI api = new CyberSourceAPI() ;
+		CyberSourceAPI.setHost( apiHost ) ;
+		CyberSourceAPI.setKey( merchantKeyId ) ;
+		CyberSourceAPI.setSecret(merchantsecretKey ) ;
+		CyberSourceAPI.setMerchant( merchantId ) ;
+
+		AuthRequest auth = new AuthRequest() ;
+		auth.reference = "wilsonhuang01" ;
+		auth.billToFirstName = "John" ;
+		auth.billToLastName = "Doe" ;
+		auth.billToAddress = "123 N. Main St." ;
+		auth.billToCity = "New York" ;
+		auth.billToState = "NY" ;
+		auth.billToZipCode = "10001" ;
+		auth.billToPhone = "(212) 123-9876" ;
+		auth.billToEmail = "john.doe@email.com" ;
+		auth.transactionAmount = "144.00" ;
+		auth.transactionCurrency = "USD" ;
+		auth.cardNumnber = "4111-1111-1111-1111" ;
+		auth.cardExpMonth = "10" ;
+		auth.cardExpYear = "2024" ;
+		auth.cardCVV = "123" ;
+		auth.cardType = "001" ;
+		boolean authValid = false ;
+		AuthResponse authResponse = new AuthResponse() ;
+		System.out.println("\n\nAuth Request: " + auth.toJson() ) ;
+		authResponse = api.authorize(auth) ;
+		System.out.println("\n\nAuth Response: " + authResponse.toJson() ) ;
+		if ( authResponse.status.equals("AUTHORIZED") ) {
+			authValid = true ;
+		}
+		
+		boolean captureValid = false ;
+		CaptureRequest capture = new CaptureRequest() ;
+		CaptureResponse captureResponse = new CaptureResponse() ;
+		if ( authValid ) {
+			capture.reference = "wilsonhuang01" ;
+			capture.paymentId = authResponse.id ;
+			capture.transactionAmount = "144.00" ;
+			capture.transactionCurrency = "USD" ;
+			System.out.println("\n\nCapture Request: " + capture.toJson() ) ;
+			captureResponse = api.capture(capture) ;
+			System.out.println("\n\nCapture Response: " + captureResponse.toJson() ) ;
+			if ( captureResponse.status.equals("PENDING") ) {
+				captureValid = true ;
+			}
+
+		}
+	
+		RefundRequest refund = new RefundRequest() ;
+		RefundResponse refundResponse = new RefundResponse() ;		
+		boolean refundValid = false ;
+
+		if ( captureValid ) {
+			refund.reference = "wilsonhuang01" ;
+			refund.captureId = captureResponse.id ;
+			refund.transactionAmount = "144.00" ;
+			refund.transactionCurrency = "USD" ;
+			System.out.println("\n\nRefund Request: " + refund.toJson() ) ;
+			refundResponse = api.refund(refund) ;
+			System.out.println("\n\nRefund Response: " + refundResponse.toJson() ) ;
+			if ( refundResponse.status.equals("PENDING") ) {
+				refundValid = true ;
+			}
+		}
+	
+
+    }	
+
+}
